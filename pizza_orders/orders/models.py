fba48991c86fa1bf8bd70de1e8f667402f735cf1
sampada109 <sampadakaushik2518@gmail.com>
2024-06-17 18:14:44 +0530

    2nd commit - making db models

--- pizza_orders/orders/models.py
+++ pizza_orders/orders/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
 
 # Create your models here.
+class BaseModel(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+# class PizzaCategory(BaseModel):
+#     category_name = models.CharField(max_length=100)
+
+
+class Size(BaseModel):
+    size = models.CharField(max_length=20)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Topping(models.Model):
+    topping_name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Pizza(BaseModel):
+    CATEGORY_CHOICE = [
+        ('Veg', 'Veg'), ('Non-Veg', 'Non-Veg')
+    ]
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+
+class Order(BaseModel):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'), ('Completed', 'Completed')
+    ]
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+
+
+class OrderItem(BaseModel):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    topping = models.ManyToManyField(Topping, blank=True)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
