d3ae91c84176db39fe677680a7239c08d228996a
sampada109 <sampadakaushik2518@gmail.com>
2024-06-28 23:04:04 +0530

    6th commit - login and register pages

--- pizza_orders/orders/urls.py
+++ pizza_orders/orders/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),  # Pass the view function without parentheses
+    path('login/', customer_login, name='customer_login'),
+    path('register/', customer_register, name='customer_register'),
 ]
--- pizza_orders/orders/views.py
+++ pizza_orders/orders/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
@@ -7,3 +10,58 @@ from .models import *
 def index(request):
     menu = Pizza.objects.all()
     return render(request, 'index.html', {'menu':menu})
+
+
+def customer_login(request):
+    if request.method == 'POST':
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user_obj = User.objects.filter(username = username)
+
+            if not user_obj.exists():
+                messages.error(request, 'user not found!')
+                return redirect('customer_register')
+
+            user_obj = authenticate(username = username, password = password)
+
+            if user_obj:
+                login(request, user_obj)
+                return redirect('/')
+            else:
+                messages.error(request, 'Invalid Credentials')
+                return redirect('customer_login')
+            
+        except Exception as e:
+            messages.error(request, e)
+            return redirect('customer_login')
+        
+    return render(request, 'login.html')
+
+
+def customer_register(request):
+    if request.method == 'POST':
+        try:
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+
+            user_obj = User.objects.filter(username = username)
+
+            if user_obj.exists():
+                messages.error(request, 'username already exists!')
+                return redirect('customer_register')
+            
+            user_obj = User.objects.create(username = username, email = email)
+            user_obj.set_password(password)
+            user_obj.save()
+
+            messages.success(request, 'user successfully created!')
+
+            return redirect('/')
+        except Exception as e:
+            messages.error(request, e)
+            return redirect('customer_register')
+        
+    return render(request, 'register.html')
