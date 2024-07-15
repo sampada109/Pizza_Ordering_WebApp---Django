693ded95071509921d243480bad9f32c3ff28c11
sampada109 <sampadakaushik2518@gmail.com>
2024-07-15 23:04:07 +0530

    25th commit path of requirements and app in build.sh

--- pizza_orders/build.sh
+++ pizza_orders/build.sh
@@ -2,11 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Install dependencies
-pip install -r ../requirements.txt
-
 # Navigate to the pizza_orders directory
 cd pizza_orders
+
+# Install dependencies
+pip install -r ../requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
