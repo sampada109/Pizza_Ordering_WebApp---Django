028f4b2db60c452ad85917f40c9943b5cfb7a675
sampada109 <sampadakaushik2518@gmail.com>
2024-07-16 20:09:28 +0530

    32th commit updating build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 # Exit on error
 set -o errexit
 
-# Navigate to the pizza_orders directory
-cd pizza_orders
-
-# Install dependencies
-pip install -r ../requirements.txt
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
