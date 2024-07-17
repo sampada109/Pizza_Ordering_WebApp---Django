ac104bd888abef1ec8e2a5184aa9e3185f40c163
sampada109 <sampadakaushik2518@gmail.com>
2024-07-17 13:45:52 +0530

    34th commit - media files on production

--- pizza_orders/pizza_orders/urls.py
+++ pizza_orders/pizza_orders/urls.py
@@ -27,4 +27,7 @@ urlpatterns = [
 
     path("__reload__/", include("django_browser_reload.urls")),   #only for development purpose
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
